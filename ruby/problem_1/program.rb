f086f1690e7d254878ab4502ce0979bda5c16d8d
Mike Fulcher <mike@plan9design.co.uk>
2013-01-04 17:40:12 +1300

    Update ruby/problem_1/program.rb

--- ruby/problem_1/program.rb
+++ ruby/problem_1/program.rb
@@ -1,8 +1,5 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-# The sum of these multiples is 23.
-# 
-# Find the sum of all the multiples of 3 or 5 below 1000.
-
+# Title: Multiples of 3 and 5
+# URL: http://projecteuler.net/problem=1
 # Usage: execute `ruby problem_1/program.rb`.
 
 # Returns true if the number is a multiple of 3.
